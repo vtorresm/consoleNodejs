d6cb62bcc6b57bb5c23c032ad117a945b8441175
Victor Torres <vatorresm88@gmail.com>
2023-01-02 12:46:17 -0500

    Agregando prueba de conexion a postgres 1

--- prueba1.js
+++ prueba1.js
@@ -1,47 +1,62 @@
-const readline = require('readline');
-const {Client} = require('pg');
+// Importación de módulos
+import readline from 'readline';
+import chalk from 'chalk';
+import pkg from 'pg';
 
-// Configuración de la conexión a PostgreSQL
-const client = new Client({
+const { Client } = pkg;
+
+// Configuración de la base de datos PostgreSQL
+const dbConfig = {
   user: 'postgres',
-  host: 'localhost',
-  database: 'pruebas',
   password: 'root',
+  host: 'localhost',
   port: 5432,
-});
+  database: 'pruebas',
+};
 
-// Función para leer datos desde la consola
-function promptUserForData(promptText) {
+// Creación del cliente PostgreSQL
+const dbClient = new Client(dbConfig);
+
+// Función para leer datos de la consola
+function promptForData(promptMessage) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
   return new Promise((resolve) => {
-    rl.question(promptText, (answer) => {
+    rl.question(promptMessage, (data) => {
       rl.close();
-      resolve(answer);
+      resolve(data);
     });
   });
 }
 
+// Función principal
 async function main() {
+  console.log(chalk.green.bold('Bienvenido al programa de consola'));
+
+  const nombre = await promptForData('Ingresa tu nombre: ');
+  const edad = await promptForData('Ingresa tu edad: ');
+
   try {
-    await client.connect();
-    console.log('Conectado a la base de datos PostgreSQL');
+    // Conexión a la base de datos
+    await dbClient.connect();
 
-    const nombre = await promptUserForData('Ingresa un nombre: ');
-    const edad = await promptUserForData('Ingresa la edad: ');
+    // Insertar datos en la base de datos
+    const insertQuery = 'INSERT INTO personas (nombre, edad) VALUES ($1, $2)';
+    await dbClient.query(insertQuery, [nombre, edad]);
 
-    const query = 'INSERT INTO personas (nombre, edad) VALUES ($1, $2)';
-    await client.query(query, [nombre, edad]);
-    console.log('Datos guardados en la base de datos.');
-
-    await client.end();
-    console.log('Desconectado de la base de datos PostgreSQL');
+    console.log(chalk.green('Datos guardados en la base de datos.'));
   } catch (error) {
-    console.error('Error:', error);
+    console.error(chalk.red('Error al guardar los datos:', error));
+  } finally {
+    // Cerrar la conexión a la base de datos
+    await dbClient.end();
   }
 }
 
-main();
+// Ejecutar la función principal
+main().catch((error) => {
+  console.error(chalk.red('Error:', error));
+});
