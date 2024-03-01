7af0fe39d28588b721eacaa9e423923b1c5e5a56
Victor <vatorresm88@gmail.com>
2024-02-29 23:21:42 -0500

    Creando utilitario para cambiar extensiones

---
+++ utils/changeExtension.js
@@ -0,0 +1,59 @@
+import fs from 'fs'
+import readline from 'readline'
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import path from 'path'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question(chalk.green('Por favor, introduce la ruta del directorio: '), (directorio) => {
+    fs.readdir(directorio, (err, archivos) => {
+        if (err) {
+            console.error(chalk.red(err))
+            returclearn
+        }
+
+        const extensiones = [...new Set(archivos.map(archivo => path.extname(archivo)))]
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'extension',
+                message: '¿Qué extensión de archivo te gustaría cambiar?',
+                choices: extensiones
+            },
+            {
+                type: 'input',
+                name: 'nuevaExtension',
+                message: 'Introduce la nueva extensión:',
+            }
+        ]).then((answers) => {
+            archivos.forEach((archivo) => {
+                const extension = path.extname(archivo)
+
+                if (extension === answers.extension) {
+                    const nombreArchivo = path.basename(archivo, extension)
+                    const nuevoNombre = `${directorio}\\${nombreArchivo}.${answers.nuevaExtension}`
+
+                    fs.rename(
+                        path.join(directorio, archivo),
+                        nuevoNombre,
+                        (err) => {
+                            if (err) {
+                                console.error(chalk.red(err))
+                                return
+                            }
+
+                            console.log(chalk.green(`Se ha cambiado la extensión de ${archivo} a ${nombreArchivo}.${answers.nuevaExtension}`))
+                        }
+                    )
+                }
+            })
+        })
+    })
+
+    rl.close()
+})
