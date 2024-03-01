f9256f1c9df09b071fb454613e0a4ae78f9267ea
Victor <vatorresm88@gmail.com>
2024-02-29 23:19:55 -0500

    Adecuando utils y pidiendo por consola una ruta

--- utils/upper.js
+++ utils/upper.js
@@ -1,30 +1,38 @@
-//const fs = require("fs");
 import fs from 'fs'
+import readline from 'readline'
+import chalk from 'chalk'
 
-const directorio = "C:\\NOC\\01.Produccion\\Scripts\\Nueva carpeta\\Funciones"; // Reemplaza por la ruta real
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-fs.readdir(directorio, (err, archivos) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+rl.question(chalk.green('Por favor, introduce la ruta del directorio: '), (directorio) => {
+  fs.readdir(directorio, (err, archivos) => {
+    if (err) {
+      console.error(chalk.red(err))
+      return
+    }
 
-  archivos.forEach((archivo) => {
-    const nombreArchivo = archivo.split(".")[0];
-    const extension = archivo.split(".")[1];
-    const nombreArchivoMayusculas = nombreArchivo.toUpperCase();
+    archivos.forEach((archivo) => {
+      const nombreArchivo = archivo.split('.')[0]
+      const extension = archivo.split('.')[1]
+      const nombreArchivoMayusculas = nombreArchivo.toUpperCase()
 
-    fs.rename(
-      `${directorio}\\${archivo}`,
-      `${directorio}\\${nombreArchivoMayusculas}.${extension}`,
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
+      fs.rename(
+        `${directorio}\\${archivo}`,
+        `${directorio}\\${nombreArchivoMayusculas}.${extension}`,
+        (err) => {
+          if (err) {
+            console.error(chalk.red(err))
+            return
+          }
+
+          console.log(chalk.green(`Se ha cambiado el nombre a ${nombreArchivoMayusculas}.${extension}`))
         }
+      )
+    })
+  })
 
-        console.log(`Se ha cambiado el nombre a ${nombreArchivoMayusculas}.${extension}`);
-      }
-      );
-  });
-});
+  rl.close()
+})
