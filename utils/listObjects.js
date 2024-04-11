4f9e14acc9b4a403dc29c6c4465ef3acdb798cc7
Victor Torres <vatorresm88@gmail.com>
2024-04-11 14:36:12 -0500

    Mejorando codigo

--- utils/listObjects.js
+++ utils/listObjects.js
@@ -3,30 +3,54 @@ import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
 
+// Función principal
 async function listFiles() {
+  // Preguntas para el usuario
   const questions = [
-    { name: 'directory', message: 'Ingrese la ruta del directorio:', validate: input => input ? true : 'Este campo es obligatorio.' },
-    { name: 'startDate', message: 'Ingrese la fecha de inicio (YYYY-MM-DD):', validate: input => input ? true : 'Este campo es obligatorio.' },
-    { name: 'endDate', message: 'Ingrese la fecha de fin (YYYY-MM-DD):', validate: input => input ? true : 'Este campo es obligatorio.' },
+    {
+      name: 'directory',
+      message: 'Ingrese la ruta del directorio:',
+      validate: (input) => (input ? true : 'Este campo es obligatorio.'),
+    },
+    {
+      name: 'startDate',
+      message: 'Ingrese la fecha de inicio (YYYY-MM-DD):',
+      validate: (input) => (input ? true : 'Este campo es obligatorio.'),
+    },
+    {
+      name: 'endDate',
+      message: 'Ingrese la fecha de fin (YYYY-MM-DD):',
+      validate: (input) => (input ? true : 'Este campo es obligatorio.'),
+    },
   ];
 
+  // Obtener respuestas del usuario
   const answers = await inquirer.prompt(questions);
 
+  // Convertir las fechas de inicio y fin a objetos Date
   const startDate = new Date(answers.startDate);
   const endDate = new Date(answers.endDate);
 
+  // Leer el directorio
   fs.readdir(answers.directory, (err, files) => {
     if (err) {
       console.log(chalk.red('Error al leer el directorio: ' + err));
     } else {
-      console.log(chalk.green('Archivos en el directorio dentro del rango de fechas:'));
-      files.forEach(file => {
+      console.log(
+        chalk.green('Archivos en el directorio dentro del rango de fechas:')
+      );
+      // Iterar sobre los archivos
+      files.forEach((file) => {
         const filePath = path.join(answers.directory, file);
+        // Obtener las estadísticas del archivo
         fs.stat(filePath, (err, stats) => {
           if (err) {
-            console.log(chalk.red('Error al obtener las estadísticas del archivo: ' + err));
+            console.log(
+              chalk.red('Error al obtener las estadísticas del archivo: ' + err)
+            );
           } else {
             const modifiedDate = new Date(stats.mtime);
+            // Verificar si la fecha de modificación está dentro del rango especificado
             if (modifiedDate >= startDate && modifiedDate <= endDate) {
               console.log(chalk.blue(file));
             }
@@ -37,4 +61,5 @@ async function listFiles() {
   });
 }
 
+// Ejecutar la función principal
 listFiles();
